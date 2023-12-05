ff53d3db57ad320474f745f3844e2187fb4f6a77
Michael Burgess <michael@Michaels-MacBook-Pro.local>
2023-12-05 10:51:56 +0000

    update

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+
+namespace GameStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/games")]
+    public class GamesController : ControllerBase
+    {
+        private readonly List<Game> games = new List<Game>
+        {
+            new Game("Super Mario 64", "Join Mario in his 3D adventure to rescue Princess Peach from Bowser's clutches.", 19.99),
+            new Game("The Legend of Zelda: Ocarina of Time", "Embark on a legendary quest as Link to stop Ganondorf and save Hyrule.", 24.99),
+            new Game("Final Fantasy VII", "Experience the epic journey of Cloud Strife and his allies to stop Sephiroth and save the planet.", 29.99),
+            new Game("Metal Gear Solid", "Infiltrate Shadow Moses Island and thwart the plans of the rogue terrorist group, FOXHOUND.", 17.99),
+            new Game("Resident Evil 2", "Survive a zombie apocalypse in Raccoon City as Leon Kennedy and Claire Redfield.", 14.99),
+            // Add more games here
+        };
+
+        [HttpGet]
+        public IActionResult GetGames()
+        {
+            return Ok(games);
+        }
+    }
+
+    record Game(
+        string Title,
+        string Description,
+        double Price
+    );
+}
